20f0587907c88b4a94241f5642b2966d14ae98ac
MadFitch <madisontfichtner@msn.com>
2017-09-27 15:24:40 -0600

    Update Vector3DTest.java

--- src/esof322/pa1/Vector3DTest.java
+++ src/esof322/pa1/Vector3DTest.java
@@ -5,49 +5,78 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Vector3DTest {
+	double f = 3;
+    double x = 2;
+    double y = 1;
+    double z = 0;
 
 	@Test
 	public void testScale() {
-	int f = 3;
-        int x = 2;
-        int y = 1;
-        int z = 0;
-        
         Vector3D expectedVector = new Vector3D(6,3,0);
         Vector3D actualVector = new Vector3D(x,y,z);
         actualVector = actualVector.scale(f);
-        assertEquals(expectedVector, actualVector);
+        
+        assertTrue(actualVector.equals(expectedVector));
 	}
 
 	@Test
 	public void testAdd() {
-		
-		fail("Not yet implemented");
+		Vector3D addingVector = new Vector3D(f, f, f);   //Adding vector (3,3,3)
+		Vector3D actualVector = new Vector3D(x, y, z);   //Vector being added to (2,1,0)
+		Vector3D expectedVector = new Vector3D(5, 4, 3); //Expected result of (2+3, 1+3, 0+3)
+		actualVector = actualVector.add(addingVector);
+
+		assertTrue(actualVector.equals(expectedVector));
 	}
 
 	@Test
 	public void testSubtract() {
-		fail("Not yet implemented");
+		Vector3D subtractingVector = new Vector3D(f, f, f);
+		Vector3D actualVector = new Vector3D(x, y, z);
+		Vector3D expectedVector = new Vector3D(-1, -2, -3);
+		actualVector = actualVector.subtract(subtractingVector);
+		
+		assertTrue(actualVector.equals(expectedVector));
 	}
 
 	@Test
 	public void testNegate() {
-		fail("Not yet implemented");
+		Vector3D actualVector = new Vector3D(x, y, z);
+		Vector3D expectedVector = new Vector3D(-x, -y, -z);
+		actualVector = actualVector.negate();
+		
+		assertTrue(actualVector.equals(expectedVector));
 	}
 
 	@Test
 	public void testDot() {
-		fail("Not yet implemented");
+		Vector3D dotVector = new Vector3D(f, f, f);
+		Vector3D actualVector = new Vector3D(x, y, z);
+		double actualDotProduct = actualVector.dot(dotVector);
+		double expectedDotProduct = ((f*x) + (f*y) + (f*z));
+		
+		assertTrue(expectedDotProduct == actualDotProduct);
 	}
 
 	@Test
 	public void testMagnitude() {
-		fail("Not yet implemented");
+		Vector3D actualVector = new Vector3D(x, y, z);
+		double actualVectorMagnitude = actualVector.magnitude();
+		double expectedVectorMagnitude = Math.sqrt(5);
+		assertTrue(expectedVectorMagnitude == actualVectorMagnitude);
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		Vector3D actualVector = new Vector3D(x, y, z);
+		String actualVectorString = actualVector.toString();
+		String expectedVectorString = "[ 2.0, 1.0, 0.0 ]";
+		assertEquals(expectedVectorString, actualVectorString);
+	}
+	
+	@Test
+	public void equals() {
+		
 	}
 
 }
