e04694d555a47c1b9f9d3e539a3db3892f0b6793
MadFitch <madisontfichtner@msn.com>
2017-09-26 20:12:41 -0600

    Update Vector3DTest.java

--- src/esof322/pa1/Vector3DTest.java
+++ src/esof322/pa1/Vector3DTest.java
@@ -14,5 +14,22 @@ public class Vector3DTest
     public static void main(String[] args) 
     {
         // TODO code application logic here
+        boolean testingScale = testScale();
+    }
+    
+            
+    //I haven't actually run any of this code as the machine I'm on doesn't even have java, so bear with me
+    //but after minimal research this looks like it is kind of what we're looking for if anyone is confused
+    public boolean testScale()
+    {
+        int f = 3;
+        int x = 2;
+        int y = 1;
+        int z = 0;
+        
+        Vector3D expectedVector = new Vector3D(6,3,0);
+        Vector3D actualVector = new Vector3D(2,1,0);
+        actualVector = actualVector.scale(f);
+        assertEquals(expectedVector, actualVector);
     }
 }
