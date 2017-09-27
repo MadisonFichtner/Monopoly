2c6530178c8ca0a9bbfb4638da53da4a0d724c89
MadFitch <madisontfichtner@msn.com>
2017-09-27 11:34:53 -0600

    Updating Vector3DTest to be a junit case
    It looks like scale is tested correctly, still have the rest todo

--- src/esof322/pa1/Vector3DTest.java
+++ src/esof322/pa1/Vector3DTest.java
@@ -1,35 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package esof322.pa1;
 
-/**
- *
- * @author q68p799
- */
-public class Vector3DTest 
-{
-    public static void main(String[] args) 
-    {
-        // TODO code application logic here
-        boolean testingScale = testScale();
-    }
-    
-            
-    //I haven't actually run any of this code as the machine I'm on doesn't even have java, so bear with me
-    //but after minimal research this looks like it is kind of what we're looking for if anyone is confused
-    public boolean testScale()
-    {
-        int f = 3;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Vector3DTest {
+
+	@Test
+	public void testScale() {
+		int f = 3;
         int x = 2;
         int y = 1;
         int z = 0;
         
         Vector3D expectedVector = new Vector3D(6,3,0);
-        Vector3D actualVector = new Vector3D(2,1,0);
+        Vector3D actualVector = new Vector3D(x,y,z);
         actualVector = actualVector.scale(f);
         assertEquals(expectedVector, actualVector);
-    }
+	}
+
+	@Test
+	public void testAdd() {
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNegate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMagnitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
 }
+
