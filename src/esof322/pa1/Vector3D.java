f53c49e7775f2563b3f6f96b2b99050385a25b8a
MadFitch <madisontfichtner@msn.com>
2017-09-27 15:23:55 -0600

    Update Vector3D.java

--- src/esof322/pa1/Vector3D.java
+++ src/esof322/pa1/Vector3D.java
@@ -23,28 +23,28 @@ public final class Vector3D
     //Vector3D	scale(double f); which should multiply x, y, and z by a common factor f.
     public Vector3D scale(double f)
     {
-        return new Vector3D(x*f,y*f,z*f);
+        return new Vector3D(this.x*f, this.y*f, this.z*f);
     }
     
     
     //which takes one Vector3D as an argument adds the corresponding coordinates to its own and produces a new Vector3D({x0, y0, z0}+{x1, y1, z1}={x0+x1,y0+y1,z0+z1},where x0, y0, and z0 are "this" object's coordinates and x1, y1, and z1 are the argument v's coordinates)	
     public Vector3D add(Vector3D v)
     {
-        return new Vector3D(x+v.x ,y+v.y ,z+v.z);
+        return new Vector3D(this.x+v.x, this.y+v.y, this.z+v.z);
     }
     
     
     //Like add except you subtract argument v's coordinates from the corresponding coordinates in "this" producing a new Vector3D object.	
     public Vector3D subtract(Vector3D v)
     {
-        return new Vector3D(x-v.x ,y-v.y ,z-v.z);
+        return new Vector3D(this.x-v.x, this.y-v.y, this.z-v.z);
     }
     
     
     //This is shorthand for scale by -1
     public Vector3D negate()
     {
-        return new Vector3D(-x,-y,-z);
+        return new Vector3D(-this.x, -this.y, -this.z);
     }
     
 
@@ -61,14 +61,24 @@ public final class Vector3D
     public double magnitude()
     {
     	double magnitude;
-    	magnitude = Math.sqrt((this.x * 2) + (this.y * 2) + (this.z * 2));
+    	magnitude = Math.sqrt((this.x * this.x) + (this.y * this.y) + (this.z * this.z));
         return magnitude;
     }
     
     
     public String toString()
     {
-        return "";
+    	String vector;
+    	vector = "[ " + this.x +", " + this.y + ", " + this.z + " ]";
+        return vector;
+    }
+    
+    public boolean equals(Vector3D v)
+    {
+    	if(this.x == v.x && this.y == v.y && this.z == v.z)
+			return true;
+		else
+			return false;
     }
     
     
