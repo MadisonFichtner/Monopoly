4daa154b2ae648f2d6ebfa49219488ceb41ce7c4
Madison Fichtner <madisontfichtner@msn.com>
2017-11-12 11:22:52 -0700

    added one more test

--- ESOF322_monopoly/test/testing/DeedTest.java
+++ ESOF322_monopoly/test/testing/DeedTest.java
@@ -6,8 +6,12 @@ import org.junit.Test;
 
 public class DeedTest {
 
+	/*
+	 * Calculate rent on a property w/o houses/hotels then with houses and hotels
+	 * and compare the results
+	 */
 	@Test
-	public void test() {
+	public void test_calculate_rent() {
 		fail("Not yet implemented");
 	}
 
