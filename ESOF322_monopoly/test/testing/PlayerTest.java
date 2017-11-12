c390361a1828ef4f52576ee0fa5a7137a9a5f10f
Madison Fichtner <madisontfichtner@msn.com>
2017-11-12 15:57:23 -0700

    added some tests

--- ESOF322_monopoly/test/testing/PlayerTest.java
+++ ESOF322_monopoly/test/testing/PlayerTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import test.Deed;
+import test.Player;
+
 public class PlayerTest {
 
 	/*
@@ -12,15 +15,34 @@ public class PlayerTest {
 	 */
 	@Test
 	public void test_bought_property() {
-		fail("Not yet implemented");
+		Player test_player = new Player("test", "player");
+		boolean valid = false;
+		Deed test_deed = new Deed(0, "test", 0, "blue", 100, 100, 10, 15, 20, 25, 30, 35, 50, "test");
+		test_player.bought_property(test_deed);
+		if(test_player.money == 1400 && test_deed.owner == test_player && test_player.deeds.get(0) == test_deed)
+			valid = true;
+		else 
+			valid = false;
+		assertEquals(valid, true);
 	}
 	
 	/*
-	 * 
+	 * Roll dice x number of times, if its between 1-12 each time, return true
 	 */
 	@Test
 	public void test_roll_dice() {
-		fail("Not done");
+		boolean valid = false;
+		Player test_player = new Player("test", "player");
+		for(int i = 0; i < 50; i++) {
+			test_player.roll_dice();
+			if(test_player.dice_sum >= 2 && test_player.dice_sum <= 12)
+			{
+				valid = true;
+			}
+			else
+				valid = false;
+		}
+		assertEquals(valid, true);
 	}
 	
 	/*
@@ -29,7 +51,12 @@ public class PlayerTest {
 	 */
 	@Test
 	public void test_move() {
-		fail("Not done");
+		Player test_player = new Player("test", "player");
+		test_player.dice_sum = 6;
+		int expected_position = 6;
+		test_player.move();
+		
+		assertEquals(test_player.position, expected_position);
 	}
 	
 	/*
@@ -37,7 +64,16 @@ public class PlayerTest {
 	 */
 	@Test
 	public void test_mortgage_deed() {
-		fail("Not done");
+		Player test_player = new Player("test", "player");
+		boolean valid = false;
+		Deed test_deed = new Deed(0, "test", 0, "blue", 100, 100, 10, 15, 20, 25, 30, 35, 50, "test");
+		test_player.mortgage_deed(test_deed);
+		if(test_player.mortgage_owed == test_deed.mortgage_value && test_player.money == 1500 + test_deed.mortgage_value && test_deed.mortgaged == true)
+			valid = true;
+		else
+			valid = false;
+		
+		assertEquals(valid, true);
 	}
 	
 	/*
@@ -46,7 +82,17 @@ public class PlayerTest {
 	 */
 	@Test
 	public void test_pay_rent() {
-		fail("Not done");
+		Player test_player = new Player("test", "player");
+		boolean valid = false;
+		Deed test_deed = new Deed(0, "test", 0, "blue", 100, 100, 10, 15, 20, 25, 30, 35, 50, "street");
+		
+		//testing if rent is correctly calculated for no houses/hotels
+		test_player.pay_rent(test_deed);
+		if(test_deed.current_rent == test_deed.rent)
+			valid = true;
+		else 
+			valid = false;
+		assertEquals(valid, true);
 	}
 	
 	/*
@@ -55,7 +101,12 @@ public class PlayerTest {
 	 */
 	@Test
 	public void test_calculate_net_worth() {
-		fail("Not done");
+		Player test_player = new Player("test", "player");
+		test_player.money += 500;
+		int expected_net_worth = 2000;
+		test_player.calculate_net_worth();
+		
+		assertTrue(test_player.overall_net_worth == expected_net_worth);
 	}
 	
 
