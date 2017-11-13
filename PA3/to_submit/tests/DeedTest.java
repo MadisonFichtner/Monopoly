682006ed2bf37901a63cefe70f3246848120c670
trent <b.trent5@gmail.com>
2017-11-12 22:32:51 -0700

    submission

---
+++ PA3/to_submit/tests/DeedTest.java
@@ -0,0 +1,18 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeedTest {
+
+	/*
+	 * Calculate rent on a property w/o houses/hotels then with houses and hotels
+	 * and compare the results
+	 */
+	@Test
+	public void test_calculate_rent() {
+		fail("Not yet implemented");
+	}
+
+}
